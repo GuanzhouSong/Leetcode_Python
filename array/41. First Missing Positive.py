590586b003025e140b92950f8dfbc73ade21c6d5
guanzhou.song@gmail.com <Song.85238799>
2018-01-06 18:29:11 +0800

    new

--- array/41. First Missing Positive.py
+++ array/41. First Missing Positive.py
@@ -4,3 +4,25 @@ class Solution:
     :type nums: List[int]
     :rtype: int
     """
+    n = len(nums)
+    i = 0
+    while i < n:
+      if nums[i] == i + 1 or nums[i] <= 0 or nums[i] > n:
+        i += 1
+      elif nums[nums[i]-1] != nums[i]:
+        self.swap(nums, i, nums[i] - 1)
+      else:
+        i += 1
+    i = 0
+    while i < n and nums[i] == i+1:
+        i+=1
+    return i+1
+
+  def swap(self, A, i, j):
+    temp = A[i]
+    A[i] = A[j]
+    A[j] = temp
+
+
+solution = Solution()
+print(solution.firstMissingPositive(nums=[1,1]))
---
+++ array/48. Rotate Image.py
@@ -0,0 +1,31 @@
+class Solution:
+  def rotate(self, matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: void Do not return anything, modify matrix in-place instead.
+    """
+    matrix = self.rotate_45(matrix)
+    matrix = self.rotate180(matrix)
+    return matrix
+
+  def rotate_45(self, matrix):
+    size = len(matrix)
+    for i in range(0, len(matrix)):
+      for j in range(0, len(matrix[0])-i):
+        temp = matrix[i][j]
+        matrix[i][j] = matrix[size-1-j][size-1-i]
+        matrix[size-1-j][size-1-i] = temp
+    return matrix
+
+  def rotate180(self, matrix):
+    size = len(matrix)
+    for i in range(0, (size + 1) // 2):
+      temp = matrix[size - i - 1]
+      matrix[size - i - 1] = matrix[i]
+      matrix[i] = temp
+    return matrix
+
+
+solution = Solution()
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+print(solution.rotate(matrix))
