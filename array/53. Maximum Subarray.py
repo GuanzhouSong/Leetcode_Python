3b9eb5efac4003c86c4c0f3ce722b373aa423798
Guanzhou Song <guanzhou.song@gmail.com>
2018-01-08 11:55:52 +0800

    new

---
+++ array/53. Maximum Subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+  def maxSubArray(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    maxnum = nums[0]
+    current_sum = 0
+    for num in nums:
+      current_sum += num
+      if current_sum > maxnum:
+        maxnum = current_sum
+      if current_sum < 0:
+        current_sum = 0
+    return maxnum
+
+solution = Solution()
+nums= [-2,1,-3,4,-1,2,1,-5,4]
+print(solution.maxSubArray(nums))
---
+++ array/54. Spiral Matrix.py
@@ -0,0 +1,6 @@
+class Solution:
+  def spiralOrder(self, matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: List[int]
+    """
